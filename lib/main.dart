1a471a738cf364e7beab3eee34a4544d59b4fdea
abinkrishna <abin48515@gmail.com>
2024-12-03 17:58:00 +0530

    save

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:muxiq/HomePage.dart';
+import 'package:muxiq/LoginPage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    )
+  );
+}
